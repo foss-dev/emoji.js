2c342fde19d39df2e92bf4d8716bf91e17c03dbf
ongerit@gmail.com <ongerit@gmail.com>
2018-07-31 19:31:21 -0400

    change file structure for categories

--- src/emoji.js
+++ src/emoji.js
@@ -3,16 +3,16 @@ import emojis from "./emoji_list"
 class Emoji {
 
 	constructor(opt) {
-		
+
 		this.shortcode = false
 		this.shortcodeSize = "16px"
 
-		if(opt) {
-			if(opt.shortcode) {
+		if (opt) {
+			if (opt.shortcode) {
 				this.shortcode = opt.shortcode
 			}
 
-			if(opt.shortcodeSize) {
+			if (opt.shortcodeSize) {
 				this.shortcodeSize = opt
 					.shortcodeSize
 					.toString()
@@ -22,46 +22,46 @@ class Emoji {
 	}
 
 	useShortCode() {
-		if(this.shortcode) {
+		if (this.shortcode) {
 			const el = document.querySelectorAll("body *")
-			
+
 			el.forEach((v, k) => {
-				
-				if(!v.dataset.emoji) {
+
+				if (!v.dataset.emoji) {
 					let data = v.innerText.match(/:(.*):/m)
-					if(data) {
-						const formatted = v.innerText.replace(new RegExp("\\:(.*):","gm"), emojis[data[1]])
+					if (data) {
+						const formatted = v.innerText.replace(new RegExp("\\:(.*):", "gm"), emojis[data[1]])
 						v.style.fontStyle = "normal"
 						v.style.fontSize = this.shortcodeSize
 						v.innerHTML = formatted
 					}
-					
+
 				}
 			})
 		}
 	}
 
-    init() {
-	  const e = document.querySelectorAll("[data-emoji]")
+	init() {
+		const e = document.querySelectorAll("[data-emoji]")
 		e.forEach((v, k) => {
 			v.style.fontStyle = "normal"
 			v.innerHTML = emojis[v.dataset.emoji]
-			if(v.dataset.emojiSize) {
-				if(v.dataset.emojiSize.replace("px", "") > 0) {
+			if (v.dataset.emojiSize) {
+				if (v.dataset.emojiSize.replace("px", "") > 0) {
 					v.style.fontSize = v
-										.dataset
-										.emojiSize
-										.replace("px", "") + "px"
+						.dataset
+						.emojiSize
+						.replace("px", "") + "px"
 				}
 			}
 		})
 	}
-	
+
 	runShortCode() {
-		if(this.shortcode) {
+		if (this.shortcode) {
 			this.useShortCode()
 		}
 	}
-  }
-  
-  export default Emoji
+}
+
+export default Emoji
